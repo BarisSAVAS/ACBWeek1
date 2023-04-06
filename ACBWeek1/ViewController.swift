fa567c05d218fb6c2314348912a10745fe9da9c1
BarisSAVAS <barissavass707@gmail.com>
2023-04-06 21:31:06 +0300

    Set Progrrammaticly

---
+++ ACBWeek1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ACBWeek1
+//
+//  Created by Barış Savaş on 6.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
