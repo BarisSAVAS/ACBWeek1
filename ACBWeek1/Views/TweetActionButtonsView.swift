c801a562b73974b1fe4e2ea49bbe81ddda477fa2
BarisSAVAS <barissavass707@gmail.com>
2023-04-10 02:56:52 +0300

    Views Done
    To Do merge view and create tableview

--- ACBWeek1/Views/TweetActionButtonsView.swift
+++ ACBWeek1/Views/TweetActionButtonsView.swift
@@ -6,10 +6,67 @@
 //
 
 import UIKit
+import SnapKit
 
 class TweetActionButtonsView: UIView {
     
     // MARK: UI Components
+        //CommentButton
+    private let commentButton: UIButton = {
+        let commentButton = UIButton()
+        commentButton.setImage(UIImage(named: "CommentButton"), for: .normal)
+        return commentButton
+    }()
+        //RetweetButton
+    private let retweetButton: UIButton = {
+       let retweetButton = UIButton()
+        retweetButton.setImage(UIImage(named: "RetweetButton"), for: .normal)
+        return retweetButton
+    }()
+        //LikeButton
+    private let likeButton: UIButton = {
+       let likeButton = UIButton()
+        likeButton.setImage(UIImage(named: "LikeButton"), for: .normal)
+        return likeButton
+    }()
+        //SharedButton
+    private let sharedButton: UIButton = {
+       let sharedButton = UIButton()
+        sharedButton.setImage(UIImage(named: "ShareButton"), for: .normal)
+        return sharedButton
+    }()
     // MARK: Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(commentButton)
+        addSubview(retweetButton)
+        addSubview(likeButton)
+        addSubview(sharedButton)
+        
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     // MARK: UI Constraints
+    private func constraintsUIComponents() {
+        commentButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.equalToSuperview().offset(10)
+        }
+        retweetButton.snp.makeConstraints { make in
+            make.top.equalTo(commentButton.snp.top)
+            make.leading.equalTo(commentButton.snp.trailing).offset(25)
+        }
+        likeButton.snp.makeConstraints { make in
+            make.top.equalTo(commentButton.snp.top)
+            make.leading.equalTo(retweetButton.snp.trailing).offset(25)
+        }
+        sharedButton.snp.makeConstraints { make in
+            make.top.equalTo(commentButton.snp.top)
+            make.leading.equalTo(likeButton.snp.trailing).offset(25)
+        }
+    }
 }
--- ACBWeek1/Views/TweetView.swift
+++ ACBWeek1/Views/TweetView.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class TweetView: UIView {
     
+    // MARK: Views
     let tweetContentView = TweetContentView()
+    let tweetActionView = TweetActionView()
+    let tweetActionButtonsView = TweetActionButtonsView()
+    
+    // MARK: Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(tweetContentView)
+        addSubview(tweetActionView)
+        addSubview(tweetActionButtonsView)
+        
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
